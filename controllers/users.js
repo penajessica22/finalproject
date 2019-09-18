03270ad09dedce8850b4a0d98afa61540725f29a
Jessica Pena <penajessica123@yahoo.com>
2019-09-18 11:23:19 -0500

    current files for final project

---
+++ controllers/users.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql')
+const pool = require('../mysql/connection')
+const { handleSQLError } = require('../mysql/error')
+
+// Select all fields from the users table.
+// limit the results to 50.
+// Use res.json to return the results to the user.
+const getUsers = (req, res) => {
+    pool.query("SELECT * FROM users", (err, rows) => {
+      if (err) return handleSQLError(res, err)
+      return res.json(rows);
+    })
+}
+
+module.exports = {
+    getUsers
+}
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const users = require('./routers/users')
+const port = process.env.PORT || 4001
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(users)
+
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`))
